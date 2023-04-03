afe8e3d999933ab7b012a2b2b52a77b5280dd715
Pedro Pinho <96132977+Pinho17@users.noreply.github.com>
2023-04-03 17:31:12 +0100

    Added src folder

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App"
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
